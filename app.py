8f68dcf357cfbbb21eaa661fe737950069159268
Patricia Akemi Oshiro <akemioshiro@hotmail.com>
2020-04-11 19:19:42 -0300

    inclusão de novas rotas

--- app.py
+++ app.py
@@ -1,14 +1,54 @@
-from flask import Flask
+import db
+from flask import Flask, abort, url_for
 
 app = Flask(__name__)
 
-@app.route('/hello')
+
+# Registro de rota usando decorator
+@app.route('/')
+def index():
+    """Retorna uma lista html com todos os users do db"""
+    html = ['<ul>']
+    for username, user in db.users.items():
+        html.append(
+            f"""
+                <li>
+                    <a href='{url_for('user', username=username)}'>
+                        {user['name']}
+                    </a>
+                </li>
+            """
+        )
+    html.append('</ul>')
+    return '\n'.join(html)
+
+
+def profile(username):
+    """Retorna um usuario especifico filtrando pelo username"""
+    user = db.users.get(username)
+
+    if user:
+        return f"""
+            <h1>{user['name']}</h1>
+            <img src="{user['image']}"/><br/>
+            telefone: {user['tel']} <br/>
+            <a href="{url_for('index')}">Voltar</a>
+        """
+    else:
+        return abort(404, "User not found")
+
+
+# Registro de rota usando chamada de método, automatiza o registro de url
+app.add_url_rule('/user/<username>/', view_func=profile, endpoint='user')
+
+# colocar sempre a barra no final, sempre que possivel
+@app.route('/hello/')
 def hello():
     return 'Hello World'
 
 
-@app.route('/hi')
+@app.route('/hi/')
 def hi():
     return 'Hi'
 
-app.run()
+app.run(use_reloader=True)
